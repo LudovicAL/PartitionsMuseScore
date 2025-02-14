557b9800fc0853e37bd7c87cf51b3e4f7284d7cd
Ludovic <ludovicaubut@hotmail.com>
2025-02-13 21:33:35 -0500

    Mise à jour de partitions

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,12 @@
+import abcjs from 'abcjs-basic-min'
+
 function buttonClick() {
    console.log("Button clicked");
+}
+
+function initEditor() {
+   new abcjs.Editor("abcTextArea", { 
+      canvas_id: "renderingDiv", 
+      warnings_id:"warningsDiv" 
+   });
 }
